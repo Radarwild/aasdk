cd04e736b49bb88cb17bcc02a6dab2ef0cda3863
Radarwild <63995815+Radarwild@users.noreply.github.com>
2022-03-11 13:25:02 +1100

    Update Data.cpp

--- src/Common/Data.cpp
+++ src/Common/Data.cpp
@@ -111,13 +111,13 @@ common::Data createData(const DataConstBuffer& buffer)
 
 std::string dump(const Data& data)
 {
-    std::string buffer;
-    boost::algorithm::hex(data, back_inserter(buffer));
-    return buffer;
+    //std::string buffer;
+    //boost::algorithm::hex(data, back_inserter(buffer));
+    //return buffer;
     return dump(DataConstBuffer(data));
 }
 
-std::string uint8_to_hex_string(const uint8_t *v, const size_t s) {
+/*std::string uint8_to_hex_string(const uint8_t *v, const size_t s) {
   std::stringstream ss;
 
   ss << std::hex << std::setfill('0');
@@ -128,7 +128,7 @@ std::string uint8_to_hex_string(const uint8_t *v, const size_t s) {
   }
 
   return ss.str();
-}
+}*/
 
 std::string dump(const DataConstBuffer& buffer)
 {
@@ -138,8 +138,8 @@ std::string dump(const DataConstBuffer& buffer)
     }
     else
     {
-        //std::string hexDump = "[" + std::to_string(buffer.size) + "] ";
-        std::string hexDump = "[" + uint8_to_hex_string(buffer.cdata, buffer.size) + " ] ";
+        std::string hexDump = "[" + std::to_string(buffer.cdata, buffer.size) + "] ";
+        //std::string hexDump = "[" + uint8_to_hex_string(buffer.cdata, buffer.size) + " ] ";
         //boost::algorithm::hex(bufferBegin(buffer), bufferEnd(buffer), back_inserter(hexDump));
         return hexDump;
     }
